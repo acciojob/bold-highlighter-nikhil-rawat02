12dc2dd2f905a45377e5e4c3c5d8887a68b8e4c9
nikhil-rawat02 <117645790+nikhil-rawat02@users.noreply.github.com>
2023-04-15 19:12:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
+let strongWords = document.getElementsByTagName("strong");
+
 function highlight() {
     //Write your code here
-
-
+	for(let word of strongWords){
+		word.style.color ="green";
+	}	
 }
-
-
 function return_normal() {
     //Write your code here
-
-    
+	for(let word of strongWords){
+		word.style.color ="black";
+	}    
 }
